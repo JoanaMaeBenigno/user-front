1737c275882ba06c186f282356f92b743a2bcc7b
JoanaMaeBenigno <joana.mae.thesis@gmail.com>
2025-04-14 22:24:43 +0800

    Rename files to lowecase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 
-import Tabs from '@/components/Tabs'
-import Footer from '@/components/Footer'
+import Tabs from '@/components/tabs'
+import Footer from '@/components/footer'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
