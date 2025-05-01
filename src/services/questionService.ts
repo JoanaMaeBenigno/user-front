4f9e93481ef90f5dd315a3e26149941285e73a45
JoanaMaeBenigno <joana.mae.thesis@gmail.com>
2025-05-01 08:45:42 +0800

    Add check learning section

---
+++ src/services/questionService.ts
@@ -0,0 +1,20 @@
+export type Choice = {
+  id: string
+  answer_text: string
+}
+
+export type Question = {
+  id: string
+  question_text: string
+  choices: Choice[]
+}
+
+export async function fetchQuestions(category_id: string): Promise<Question[]> {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+  const res = await fetch(`${API_URL}/check_learning/question/${category_id}`)
+  if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+  const json = await res.json()
+
+  return json.data || []
+}
